85dd1862a7794f57910bff049ce92953bf4c8110
Dao Ngoc <ngoc@Daos-MacBook-Pro.local>
2013-11-01 12:28:15 +0900

    Fix #3 (Delete target/xitrum directory at each run) and #4 (Support SBT multimodule (multiproject))

--- src/main/scala/XitrumPlugin.scala
+++ src/main/scala/XitrumPlugin.scala
@@ -1,4 +1,4 @@
-import java.io.FileOutputStream
+import java.io.File
 
 import sbt._
 import Keys._
@@ -14,46 +14,74 @@ object XitrumPlugin extends Plugin {
 
   // Must be lazy to avoid null error
   lazy val xitrumPackageTask = xitrumPackageKey <<=
-      (externalDependencyClasspath in Runtime, baseDirectory, target,    crossTarget) map {
-      (libs,                                   baseDir,       targetDir, jarOutputDir) =>
+    // dependencyClasspath: both internalDependencyClasspath and externalDependencyClasspath
+    // internalDependencyClasspath ex: classes directories
+    // externalDependencyClasspath ex: .jar files
+    (dependencyClasspath in Runtime, baseDirectory, target,    crossTarget) map {
+    (libs,                           baseDir,       targetDir, jarOutputDir) =>
     try {
       val packageDir = targetDir / "xitrum"
-      packageDir.mkdirs
-
-      // Copy bin directory
-      val binDir1 = baseDir / "bin"
-      val binDir2 = packageDir / "bin"
-      IO.copyDirectory(binDir1, binDir2)
-      val binDir2Files = binDir2.listFiles
-      if (binDir2Files != null) binDir2Files.foreach { _.setExecutable(true) }
-
-      // Copy config directory
-      val configDir1 = baseDir / "config"
-      val configDir2 = packageDir / "config"
-      IO.copyDirectory(configDir1, configDir2)
-
-      // Copy public directory
-      val publicDir1 = baseDir / "public"
-      val publicDir2 = packageDir / "public"
-      IO.copyDirectory(publicDir1, publicDir2)
+      deleteFileOrDirectory(packageDir)
+      packageDir.mkdirs()
 
       // Copy dependencies to lib directory
       val libDir = packageDir / "lib"
       libs.foreach { lib =>
         val file = lib.data
 
-        // Prevent copying directories in classpath, e.g. "config" directory
-        if (file.exists && !file.isDirectory) IO.copyFile(file, libDir / file.name)
+        if (file.exists) {
+          if (file.isDirectory) {
+            // This dependency may be "classes" directory from SBT multimodule (multiproject)
+            // http://www.scala-sbt.org/0.13.0/docs/Getting-Started/Multi-Project.html
+            //
+            // Ex:
+            // /Users/ngoc/src/xitrum-multimodule-demo/module1/target/scala-2.10/classes
+            // /Users/ngoc/src/xitrum-multimodule-demo/module1/target/scala-2.10/xitrum-multimodule-demo-module1_2.10-1.0-SNAPSHOT.jar
+            if (file.name == "classes") {
+              val upperDir = file / ".."
+              (upperDir * "*.jar").get.foreach { f => IO.copyFile(f, libDir / f.name) }
+            }
+          } else {
+            IO.copyFile(file, libDir / file.name)
+          }
+        }
       }
 
       // Copy .jar files created after running "sbt package" to lib directory
-      (jarOutputDir * "*.jar").get.foreach { file => IO.copyFile(file, libDir / file.name) }
+      // (see xitrumPackageNeedsPackageBin)
+      (jarOutputDir * "*.jar").get.foreach { f => IO.copyFile(f, libDir / f.name) }
 
-      println("Please see target/xitrum directory")
+      // TODO: https://github.com/ngocdaothanh/xitrum-sbt-plugin/issues/1
+      val copyFiles = Seq("bin", "config", "public")
+      copyFiles.foreach { f => copy(f, baseDir, packageDir) }
+
+      println("Packaged to " + packageDir)
     } catch {
       case e: Exception => e.printStackTrace
     }
   }
 
   val xitrumPackageNeedsPackageBin = xitrumPackageKey <<= xitrumPackageKey.dependsOn(packageBin in Compile)
+
+  //----------------------------------------------------------------------------
+
+  private def deleteFileOrDirectory(file: File) {
+    if (file.isDirectory) {
+      val files = file.listFiles
+      if (files != null) files.foreach { f => deleteFileOrDirectory(f) }
+    }
+    file.delete()
+  }
+
+  private def copy(fileName: String, baseDir: File, packageDir: File) {
+    val from = baseDir / fileName
+    if (!from.exists) return
+
+    val to = packageDir / fileName
+    if (from.isDirectory) IO.copyDirectory(from, to) else IO.copyFile(from, to)
+
+    // TODO: keep executable property (note: there may be subdirs)
+    //val files = to.listFiles
+    //if (files != null) files.foreach { _.setExecutable(true) }
+  }
 }
